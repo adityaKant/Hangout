d94a9ad6c318f92d48f25c3ed2e1c548a6f778c5
adityaKant <adikant9@gmail.com>
2017-03-27 13:57:37 -0400

    port added for prod

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.get('*', function(req, res) {
 });
 
 // listen (start app with node server.js) ======================================
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("App listening on port 3000");
