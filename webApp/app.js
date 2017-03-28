ac9cf2c7b2c40a0c7f4fc1cd34233393955f7766
adityaKant <adikant9@gmail.com>
2017-03-28 02:58:55 -0400

    directive for search bar

--- webApp/app.js
+++ webApp/app.js
@@ -4,7 +4,8 @@
     var app = angular.module('hangout',[
         'hangout.routes',
         'hangout.headerController',
-        'ngMaterial'
+        'ngMaterial',
+        'SearchBar.directive'
     ]);
 
 
