6c0aba8ec80a263c0d2321da9188b45fb1856ac2
adityaKant <adikant9@gmail.com>
2017-03-29 21:09:39 -0400

    Merge branch 'OracleDB'

--- server.js
+++ server.js
@@ -2,13 +2,18 @@
 //  setup
 var express  = require('express');
 var app      = express();
+var oracledb = require('oracledb');
 
 app.use(express.static('./webApp'));
 
 // application -------------------------------------------------------------
+app.get('/:id', function(req, res) {
+    res.sendfile(__dirname + '/webApp/start.js'); // load the single view file (angular will handle the page changes on the front-end)
+});
 app.get('*', function(req, res) {
     res.sendfile(__dirname + '/webApp/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
+
 
 // listen (start app with node server.js) ======================================
 app.listen(process.env.PORT || 3000);
