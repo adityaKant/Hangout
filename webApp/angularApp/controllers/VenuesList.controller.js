15c94ba35900befc4d10ed9e0a177c010534b3cd
adityaKant <adikant9@gmail.com>
2017-04-15 17:47:50 -0400

    pagination on venueList page

--- webApp/angularApp/controllers/VenuesList.controller.js
+++ webApp/angularApp/controllers/VenuesList.controller.js
@@ -9,14 +9,62 @@
         vm.filter.categories = [];
         vm.venues = $venueList.getVenuesList();
         vm.searchResultsFor = $stateParams.search;
+        var page = 1;
+        var per = 10;
+        var filtersApplied = false;
+        vm.nextPageDisabled = false;
+        vm.prevPageDisabled = true;
 
-        if($stateParams.search.length > 0 && vm.venues.keyword == null){
-            api.Venues.get({keyword: $stateParams.search},function (response) {
+        var getVenuesList = function(payload){
+            api.Venues.get(payload,function (response) {
                 vm.venues.venues = response.venues;
-                // vm.venues.keyword = $stateParams.search;
+                if(response.venues.length < per)
+                    vm.nextPageDisabled = true;
             },function (errResponse) {
             })
+        };
+
+        if($stateParams.search.length > 0 && vm.venues.keyword == null){
+           getVenuesList({keyword: $stateParams.search});
         }
-        console.log(vm.venues);
+        
+        vm.applyFilter = function () {
+            filtersApplied = true;
+            var payload = {
+                keyword: $stateParams.search,
+                filter : vm.filter
+            };
+            getVenuesList(payload);
+        };
+
+        vm.nextPage = function () {
+            vm.prevPageDisabled = false;
+            page++;
+            var payload = {
+                keyword : $stateParams.search,
+                page : page,
+                per : per
+            };
+            if(filtersApplied)
+                payload.filter = vm.filter;
+
+            getVenuesList(payload);
+        };
+
+        vm.prevPage = function () {
+            page--;
+            if(page == 1)
+                vm.prevPageDisabled = true;
+            vm.nextPageDisabled = false;
+            var payload = {
+                keyword : $stateParams.search,
+                page : page,
+                per : per
+            };
+            if(filtersApplied)
+                payload.filter = vm.filter;
+
+            getVenuesList(payload);
+        };
     }
 })();
