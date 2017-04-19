31cea4b3722f21eaf2ba7c07dd5f25e4444e0663
adityaKant <adikant9@gmail.com>
2017-04-19 06:16:34 -0400

    donut chart added on venue details page

--- webApp/angularApp/controllers/Venue.controller.js
+++ webApp/angularApp/controllers/Venue.controller.js
@@ -1,29 +1,48 @@
 (function() {
-    angular.module("Venue.controller", ['nvd3'])
+    angular.module("Venue.controller", [
+        'nvd3',
+        'donutChart.service'
+    ])
             .controller("VenueController", venueCtrl);
 
-        function venueCtrl(api, $stateParams) {
+        function venueCtrl(api, $stateParams, $donutChart) {
             var  vm = this;
             var venueId = $stateParams.id;
             var payload = {
                 id : venueId
-            }
+            };
+            vm.chartOptions = $donutChart.getOptions();
+
             api.Venues.get(payload, function(response) {
                 vm.venue = response.venue;
                 vm.categories = response.category.map(function(a) {return a.CAT_NAME;});;
             },
             function(errResponse) {
             });
+
             getVenueReviews();
 
             function getVenueReviews() {
                 api.Venues.reviews({id: $stateParams.id}, function (response) {
                     vm.reviews = response.reviews;
+                    vm.chartData = getData(response.rating);
                 },
                 function (errResponse) {
-                    debugger;
                 })
             }
+        }
+        function getData(ratings) {
+            var dataArray = [];
+
+            angular.forEach(ratings,function(val,index){
+                var temp = {};
+                temp.key = val.REVIEW_TYPE;
+                temp.y = val.COUNT
+
+                dataArray.push(temp);
+            });
+
+            return dataArray;
         }
 
 
