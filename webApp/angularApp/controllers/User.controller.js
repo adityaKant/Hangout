d2d3e40ef7bccfb38998f7ee09c36fa3b8f45a91
meghatiwari10 <tiwari.megha10@gmail.com>
2017-04-19 07:50:53 -0400

    userReviews modified

--- webApp/angularApp/controllers/User.controller.js
+++ webApp/angularApp/controllers/User.controller.js
@@ -6,9 +6,19 @@
         var vm = this;
         api.Me.get  ({},function (response) {
             vm.user = response
-            console.log(response)
+//            console.log(response)
 
         },function (errResponse) {
+
+        })
+
+        api.Me.myReviews({}, function(reviews) {
+            var response = [];
+            response = reviews
+            debugger
+            vm.myReviews = response[0]
+//            console.log(myReviews)
+        },function(errResponse1) {
 
         })
     }
